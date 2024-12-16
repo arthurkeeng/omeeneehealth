ad4e748cded5e2e1d066fa6807540e4fc6e6621c
keeng arthur <121835767+arthurkeeng@users.noreply.github.com>
2024-12-16 23:54:42 +0100

    Update page.jsx

--- app/patients/[id]/new-appointment/success/page.jsx
+++ app/patients/[id]/new-appointment/success/page.jsx
@@ -1,84 +1,80 @@
-import { Button } from "@/components/ui/button"
-import { Doctors } from "@/constants"
-import { getAppointment } from "@/lib/actions/appointment.action"
-import { formatDateTime } from "@/lib/utils"
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
 
+import { Button } from "@/components/ui/button";
+import { Doctors } from "@/constants";
+import { getAppointment } from "@/lib/actions/appointment.actions";
+import { formatDateTime } from "@/lib/utils";
 
-
-
-// const SuccessPage = async ({params  , searchParams} : SearchParamProps) => {
 const SuccessPage = async ({params  , searchParams} ) => {
   const {id} = await params
   const {appointmentId } = await searchParams 
-  console.log(appointmentId)
-  // const appointmentId = await (searchParams?.appointmentId as string || "")
+  const appointment = await getAppointment(appointmentId);
 
-  // const appointment = await getAppointment(appointmentId as string)
-  const appointment = await getAppointment(appointmentId)
+  const doctor = Doctors.find(
+    (doctor) => doctor.name === appointment.primaryPhysician
+  );
 
-  const doctor = Doctors.find(doc => doc.name === appointment.primaryPhysician)
   return (
-    <div className="flex h-screen max-h-screen px-[5%]">
+    <div className=" flex h-screen max-h-screen px-[5%]">
       <div className="success-img">
-        <Link href='/' className="flex align-middle">
+        <Link href="/">
           <Image
-          src='/assets/icons/medical.png'
-          height={1000}
-          width={1000}
-          alt="logo"
-          className="h-10 w-fit"
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="logo"
+            className="h-10 w-fit"
           />
-        <p className="ml-3 font-bold text-xl">OmeeneeHealth</p>
         </Link>
+
         <section className="flex flex-col items-center">
           <Image
-          src='/assets/gifs/success.gif'
-          height={300}
-          width={280}
-          alt="success"
+            src="/assets/gifs/success.gif"
+            height={300}
+            width={280}
+            alt="success"
           />
-        <h2 className="header mb-6 max-w-[600px] text-center">
-          Your <span className="text-green-500">Appointment request</span>
-          has been successfully submitted !
-        </h2>
-        <p>We will be in touch shortly to confirm appointment</p>
+          <h2 className="header mb-6 max-w-[600px] text-center">
+            Your <span className="text-green-500">appointment request</span> has
+            been successfully submitted!
+          </h2>
+          <p>We&apos;ll be in touch shortly to confirm.</p>
         </section>
+
         <section className="request-details">
           <p>Requested appointment details: </p>
           <div className="flex items-center gap-3">
             <Image
-            src={doctor.image}
-            // src={doctor?.image!}
-            alt="doctor"
-            width={100}
-            height={100}
-            className="size-6"
+              src={doctor.image!}
+              alt="doctor"
+              width={100}
+              height={100}
+              className="size-6"
             />
-            <p className="whitespace-nowrap">
-              {doctor?.name}
-            </p>
-            <div className="flex gap-2">
-              <Image
-              src='/assets/icons/calendar.svg'
+            <p className="whitespace-nowrap">Dr. {doctor.name}</p>
+          </div>
+          <div className="flex gap-2">
+            <Image
+              src="/assets/icons/calendar.svg"
               height={24}
               width={24}
               alt="calendar"
-              />
-              <p>{formatDateTime(appointment.schedule).dateTime}</p>
-            </div>
+            />
+            <p> {formatDateTime(appointment.schedule).dateTime}</p>
           </div>
         </section>
-        <Button variant='outline' className="shad-primary-btn" asChild>
-          <Link href={`/patients/${id}/new-appointment`}>
-          New Appointment</Link>
-        </Button>
-        <p className="copyright">© 2024 OmeeneeHealth</p>
-      </div>
-      
-    </div>
-  )
-}
 
-export default SuccessPage
+        <Button variant="outline" className="shad-primary-btn" asChild>
+          <Link href={`/patients/${userId}/new-appointment`}>
+            New Appointment
+          </Link>
+        </Button>
+
+        <p className="copyright">© 2024 CarePluse</p>
+      </div>
+    </div>
+  );
+};
+
+export default RequestSuccess;
